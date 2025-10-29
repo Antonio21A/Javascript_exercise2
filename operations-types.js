9765f497fe06a9b04630f05e3cdf74d11d3ba7af
Antonio21A <antoniowilliams135a@gmail.com>
2025-10-29 14:47:36 +0200

    Refactor variable names and logging in operations-types.js
    Refactor variable names and enhance console logging for clarity.

--- operations-types.js
+++ operations-types.js
@@ -3,12 +3,12 @@ const number1 = 12;
 // TODO: Create a variable called number2 and store a different number in it 
 const number2 = 8;
 // TODO: Convert number1 to a string and store it in a new variable 
-const number1AsString = number1.toString();
+const number_string = string(number1);
 // TODO: Add number1 and number2 and store the result in a variable called sum 
 const sum = number1 + number2;
 // TODO: Multiply number1 and number2 and store the result in a variable called product 
 const product = number1 * number2;
 // TODO: Print all results to the console 
-console.log("number1 as string: " + number1AsString);
-console.log("Sum: " + sum);
-console.log("Product: " + product);
+console.log("this number has been converted into a string " + number_string);
+console.log("The sum of " + number1 _ " + " + number2 + " = " + sum + " . ");
+console.log("The Product of "+ number1 + " * " + number2 +" is " + product + " . ");
