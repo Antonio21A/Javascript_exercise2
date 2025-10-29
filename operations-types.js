d2f5c4911059e6d1756ba2ffb33e8f90e213088f
Antonio21A <antoniowilliams135a@gmail.com>
2025-10-29 12:25:36 +0200

    java part 2

---
+++ operations-types.js
@@ -0,0 +1,14 @@
+// TODO: Create a variable called number1 and store any number in it
+const number1 = 12; 
+// TODO: Create a variable called number2 and store a different number in it 
+const number2 = 8;
+// TODO: Convert number1 to a string and store it in a new variable 
+const number1AsString = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum 
+const sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product 
+const product = number1 * number2;
+// TODO: Print all results to the console 
+console.log("number1 as string: " + number1AsString);
+console.log("Sum: " + sum);
+console.log("Product: " + product);
